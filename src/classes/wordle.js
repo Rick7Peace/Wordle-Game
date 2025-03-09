6ca78cf401e518e86e61e30566c6a2bcabb3cc68
your-username <marmolejo.ricardo@gmail.com>
2025-03-09 14:36:27 -0400

    last update

--- src/classes/wordle.js
+++ src/classes/wordle.js
@@ -1,8 +1,6 @@
-Wordle.js
-const GREEN = "g"
-const YELLOW = "y"
-const BLACK = "b" 
-
+const GREEN = "g";
+const YELLOW = "y";
+const BLACK = "b";
 
 class Wordle {
   word;
@@ -15,21 +13,50 @@ class Wordle {
       return [];
     }
     if (guess === this.word) {
-      return [GREEN,GREEN,GREEN,GREEN,GREEN];
+      return [GREEN, GREEN, GREEN, GREEN, GREEN];
     }
     let result = [];
+
     for (let i = 0; i < guess.length; i++) {
       if (guess[i] === this.word[i]) {
-        result.push(GREEN)
-      } else if(this.word.includes(guess[i])) {
-        result.push(YELLOW)
-      }
-       else {
-        result.push(BLACK)
+        result.push(GREEN);
+      } else if (this.word.includes(guess[i])) {
+        if (this.letterRepeatedInGuess(guess, i)) {
+          result.push(BLACK);
+        } else {
+          result.push(YELLOW);
+        }
+      } else {
+        result.push(BLACK);
       }
     }
     return result;
   }
+  
+  letterRepeatedInGuess(guess, index) {
+    let charCountInGuess = [];
+    for (let i = 0; i < guess.length; i++) {
+      if (guess[i] === guess[index]) {
+        charCountInGuess.push(i);
+      }
+    }
+    let charCountInWordle = [];
+    for (let i = 0; i < this.word.length; i++) {
+      if (this.word[i] === guess[index]) {
+        charCountInWordle.push(i);
+      }
+    }
+    if (charCountInGuess.length === 1) {
+      return false;
+    }
+    if (charCountInGuess.length === charCountInWordle.length) {
+      return false;
+    }
+    if (index === charCountInGuess[0]) {
+      return false;
+    }
+    return true;
+  }
 }
 
-module.exports = {Wordle, GREEN, YELLOW, BLACK};
+module.exports = { Wordle, GREEN, YELLOW, BLACK };
